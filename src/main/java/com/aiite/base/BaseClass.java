0d1a0bff038d48df5f107c43d913e5dffe6ad9df
lakshmanakumar37 <lakshmanakumar37@gmail.com>
2022-12-26 14:42:52 +0530

    "This is first commit"

---
+++ src/main/java/com/aiite/base/BaseClass.java
@@ -0,0 +1,39 @@
+package com.aiite.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void browserLunch() {
+		
+		if(System.getProperty("browser").equalsIgnoreCase("chrome")){
+			WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver();
+		}else if(System.getProperty("browser").equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+		}else if(System.getProperty("browser").equalsIgnoreCase("IE")) {
+			WebDriverManager.iedriver().setup();
+			driver=new EdgeDriver();
+		}
+		
+		driver.manage().window().maximize();
+		driver.get("url");
+	}
+	
+	@AfterMethod
+	public void browserClose() {
+		driver.quit();
+	}
+
+}
---
+++ src/test/java/com/aiite/testCases/TestCases.java
@@ -0,0 +1,24 @@
+package com.aiite.testCases;
+
+import org.testng.annotations.Test;
+
+public class TestCases {
+	@Test
+	public void testCase1() {
+		
+		System.out.println("This is test case1");
+		
+	}
+	@Test
+	public void testCase2() {
+		
+		System.out.println("This is test case2");
+		
+	}
+	@Test
+	public void testCase3() {
+		
+		System.out.println("This is test case3");
+		
+	}
+}
